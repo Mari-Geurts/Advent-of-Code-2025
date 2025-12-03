ee4668422171ee0f9288a26ef2b48a8699eaec2b
Mari Geurts <mgeurts72001@gmail.com>
2025-12-03 15:45:08 -0500

    skibidi

--- day1/main.py
+++ day1/main.py
@@ -1,6 +1,6 @@
 startInt = 50
 
-with open("example.txt", 'r') as file:
+with open("input.txt", 'r') as file:
     #file processing
     currentValue = startInt
     zeroTally = 0
@@ -10,9 +10,11 @@ with open("example.txt", 'r') as file:
 
         if direction[0] == 'L':
             distance = int(direction[1:])
-            print(f"Rotated Left {distance}")
 
             while distance > 0:
+                if currentValue == 0:
+                    zeroTally += 1
+
                 if currentValue <= 0:
                     currentValue = 99
 
@@ -24,20 +26,22 @@ with open("example.txt", 'r') as file:
         elif direction[0] == 'R':
 
             distance = int(direction[1:])
-            print(f"Rotated Right {distance}")
             
             while distance > 0:
+                if currentValue == 0:
+                    zeroTally += 1
+
                 if currentValue >= 99:
                     currentValue = 0
-                    #print("Turning dial to 0")
+
                 else:
                     currentValue += 1
                 distance -= 1
                 
         else:
             print(" No L or R found!")
-        if currentValue == 0:
-            zeroTally += 1
+        """if currentValue == 0:
+            zeroTally += 1"""
     print(f"Password: {zeroTally}")
 
 
