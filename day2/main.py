f577bc9bce62e9a8bc9611dac75c31a325620faa
Mari Geurts <mgeurts72001@gmail.com>
2025-12-16 17:06:13 -0500

    main.py now counts invalid Ids that are even length

--- day2/main.py
+++ day2/main.py
@@ -1,49 +1,62 @@
-#To fucking do list
-# grab first id and last id [CHECK]
-# turn firstID and lastID into a range [CHECK]
-# Increment through the range [CHECK]
-# invalid ids are when the range has repeated digits. ie `55` `6464` and `123123` [CHECK]
-#Find all invalid ids, and add them up. [CHECK]
+"""
+### To fucking do list ###
+- grab first id and last id [CHECK]
+- turn firstID and lastID into a range [CHECK]
+- Increment through the range [CHECK]
+- invalid ids are when the range has repeated digits. ie `55` `6464` and `123123` [CHECK]
+- Invalid IDs now include any repeated patterns, find even length first [CHECK]
+ - Find odd length ones [ ]
+- Find all invalid ids and add them up. [ ]
+"""
 import re
 
 with open("example.txt", "r") as file:
-    #file processing
 
     document = file.readline().rstrip()
     listIds = re.split("[-,]" ,document)
     invalidSum = 0
-    counter = 1
+    isFirstId = True
 
     for idRange in listIds:
         
-        if counter == 1: #determines if it's the firstId
+        if isFirstId == True: #determines if it's the firstId
             firstId = int(idRange)
-            counter += 1
+            isFirstId = False
 
         else: #if not firstId, must be last
             lastId = int(idRange)
             workingRange = range(firstId,lastId+1)
+            isFirstId = True
             
-            for i in workingRange: #determine here if the id is invalid or not       
-                indexLength = len(str(i))
+            for workingId in workingRange: #determine here if the id is invalid or not       
+                indexLength = len(str(workingId))
                 half = int(indexLength//2)
-
-                if indexLength % 2 == 0: # Indicates that there is an even amount of characters, Not all are invalid; we have to determine by splitting down the middle and checking if they're == 
-
-                    if str(i)[:half] == str(i)[half:]: # If first half and last half match, then they are invalid
-                        print(f"{i} is {indexLength} long. It begins with {str(i)[:int(indexLength//2)]} and ends with {str(i)[int(indexLength//2):]}")
-                        invalidSum += i
-
-                    else: #if left half != right half, proceed another method to check if invalid
-                        
-                        pass    
-
                 
-                else: #must be odd value, proceed another method to check if invalid
+                if indexLength % 2 == 0: # Indicates that there is an even amount of characters, 
+                    #Not all are invalid; we have to determine by splitting down the middle and checking if they're == 
+                    workingVarSlice = []    
+                    for i in range(0,half): # Meaning we stop at the end of the length
+                        if str(workingId)[:half-i] == str(workingId)[half+i:]: # matches
+
+                            for n in range(0,indexLength,half-i):
+                                workingVarSlice.append(str(workingId)[n:n+half-i])
+                        
+                            for chunk in workingVarSlice:
+                                
+                                if chunk == workingVarSlice[0]:
+                                    isInvalid = True
+                                
+                                else:
+                                    isInvalid = False
+                                    break
+                            if isInvalid:
+                                print(f"Current value invalid, adding {workingId}.")
+                                invalidSum+=workingId
+                
+                else: #must be odd length, proceed another method to check if invalid
                     
                     pass
                 
-            counter = 1
-    #print(f"Final sum is {invalidSum}")
+    print(f"Final sum is {invalidSum}")
 
 
